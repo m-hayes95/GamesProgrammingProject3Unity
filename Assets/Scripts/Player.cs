996b1ef5aabe2a7a161d626423b53d2615670d66
m-hayes95 <115077851+m-hayes95@users.noreply.github.com>
2022-11-02 12:22:51 +0000

    Added 2d collisions with game map and enemy
    Added 2d collisions with game map and enemy

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,17 +6,26 @@ public class Player : MonoBehaviour
 {
     public Animator myAnimator;
     public GameObject mySprites;
+    public float speed;
+    public float health;
     // Start is called before the first frame update
     void Start()
     {
         myAnimator = GetComponent<Animator>();
+        speed = 10f;
+        health = 10f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+
+        Vector3 inputFromPlayer = new Vector3(h, v, 0);
+        
+        transform.Translate(inputFromPlayer * speed * Time.deltaTime);
 
         //if (h == 0)
         {
@@ -28,6 +37,16 @@ public class Player : MonoBehaviour
             //Moving right
             //myAnimator.SetBool(“name of bool used in animator controller”, true);
             //mySprites.transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision with" + collision.gameObject.name);
+        if (collision.gameObject.CompareTag("enemy"))
+        {
+            //Player Takes Damage
+            Debug.Log("Player Takes Damage");
         }
     }
 }
