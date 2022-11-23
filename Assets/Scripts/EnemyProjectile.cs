838cf191a1e23a1d51345781c9335a9631e7ec1b
m-hayes95 <115077851+m-hayes95@users.noreply.github.com>
2022-11-23 14:44:56 +0000

    Finished Updating for UML diagrams
    Finished Updating for UML diagrams

---
+++ Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    public float enemyDamage;
+    private float enemyProjectileThrust;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private GameObject pillar;
+    private GameObject pillars;
+
     // Start is called before the first frame update
     void Start()
     {
